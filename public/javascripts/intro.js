8c76a5836af499b69ec2ab7213c8ac9ffd968798
Young <wjdtp93@gmail.com>
2021-09-24 13:32:50 +0900

    style: javascript 파일 기능별 리팩토링

---
+++ public/javascripts/intro.js
@@ -0,0 +1,7 @@
+function intro() {
+  var main = document.getElementById('main');
+  main.innerHTML = `
+  <a href="https://github.com/younGsse/scenespick" style="font-size:64px;">Github</a>
+  <p>버그 신고, 이용 문의 wjdtp93@gmail.com</p>
+  `;
+}
