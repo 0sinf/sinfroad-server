df3d48d45dd7dd484a39f4afbd13c6b5235401d3
정세영 <wjdtp93@gmail.com>
2022-05-04 10:02:10 +0900

    Fix posts entity

--- src/posts/posts.entity.ts
+++ src/posts/posts.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('Posts')
 export class PostsEntity {
@@ -13,4 +19,10 @@ export class PostsEntity {
 
   @Column()
   address: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
 }
