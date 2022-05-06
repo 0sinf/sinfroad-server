ebb084fe303406e75deb0f2042e5710917addba1
정세영 <wjdtp93@gmail.com>
2022-05-06 13:43:53 +0900

    Add exception when get post

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePostReq } from './dto/create-post.dto';
@@ -18,13 +18,27 @@ export class PostsService {
   }
 
   async getPost(postId: string) {
+    if (!this.isUuid(postId)) {
+      throw new BadRequestException('올바르지 않은 접근입니다.');
+    }
+
     const post = await this.postsRepository
       .createQueryBuilder('post')
       .leftJoinAndSelect('post.images', 'image')
       .where('id = :id', { id: postId })
       .getOne();
 
+    if (!post) {
+      throw new BadRequestException('존재하지 않는 글입니다.');
+    }
+
     return post;
+  }
+
+  private isUuid(postId: string): boolean {
+    return !!postId.match(
+      /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+    );
   }
 
   async getPosts() {
