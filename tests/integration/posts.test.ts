25ba9a727c8004522d6d1fe64d6d773bd35b0220
정세영 <wjdtp93@gmail.com>
2022-05-18 17:12:36 +0900

    Fix test code

--- tests/integration/posts.test.ts
+++ tests/integration/posts.test.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import mongoose from "mongoose";
 import { rmSync } from "fs";
 import app from "../../src/app";
+import config from "../../src/config";
 
 describe("Post integration test", () => {
   let postId: string;
@@ -10,6 +11,7 @@ describe("Post integration test", () => {
   it("POST /api/posts", async () => {
     const response = await request(app)
       .post("/api/posts")
+      .set("authorization", "Bearer " + config.secretKey)
       .field("title", "title")
       .field("contents", "contents")
       .field("address", "seoul")
