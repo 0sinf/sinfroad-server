cf4a73d5e373815bfe15bec819600240669cf759
정세영 <wjdtp93@gmail.com>
2022-05-18 17:09:51 +0900

    Add middleware

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import * as postController from "../controller/post";
 import upload from "../utils/multer";
+import checkPermission from "../middleware/check-permission";
 
 const postRouter = Router();
 
-postRouter.post("/", upload.array("images", 4), postController.createPost);
+postRouter.post(
+  "/",
+  checkPermission,
+  upload.array("images", 4),
+  postController.createPost
+);
 postRouter.get("/", postController.getPosts);
 postRouter.get("/:id", postController.getPost);
 
