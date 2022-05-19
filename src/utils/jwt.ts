1c5b258cf10bf11ce606d6da4746e38b3edd83bf
정세영 <wjdtp93@gmail.com>
2022-05-19 17:22:31 +0900

    Add jwt verify

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,3 +5,7 @@ import config from "../config";
 export function genToken(user: User) {
   return sign({ user }, config.jwtSecret, { expiresIn: "7d" });
 }
+
+export function verifyToken(token: string) {
+  return verify(token, config.jwtSecret);
+}
