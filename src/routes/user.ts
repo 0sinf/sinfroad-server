50b7ae40ddeb0a81cd761f556994216814f14ee8
정세영 <wjdtp93@gmail.com>
2022-05-19 17:02:14 +0900

    Add login in route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import * as userController from "../controller/user";
 
 const userRouter = Router();
+
+userRouter.post("/login", userController.loginUser);
 
 export default userRouter;
