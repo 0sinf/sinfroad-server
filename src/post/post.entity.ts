24a1df4347638f54c0845723755a8b7c1592b3f1
정세영 <wjdtp93@gmail.com>
2022-06-07 12:36:12 +0900

    feat: Add Post Entity

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,4 +1,31 @@
-import { Entity } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('posts')
-export class PostEntity {}
+export class PostEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  contents: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  images: string[];
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+}
