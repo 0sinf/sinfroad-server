5c94f9cd6e9f4fa17b4d7c7b99f6b43e4c1ea867
정세영 <wjdtp93@gmail.com>
2022-06-08 12:11:23 +0900

    style: Fix multer options

--- src/utils/upload-options.ts
+++ src/utils/upload-options.ts
@@ -3,10 +3,10 @@ import * as multer from 'multer';
 import { v4 as uuid } from 'uuid';
 import { existsSync, mkdirSync } from 'fs';
 import { extname } from 'path';
-import { Request } from 'express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 
-const multerOptions = {
-  fileFilter: (req: Request, file: Express.Multer.File, cb) => {
+const multerOptions: MulterOptions = {
+  fileFilter: (req, file, cb) => {
     if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
       return cb(null, true);
     }
