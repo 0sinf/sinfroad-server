6da9c85774d6a335a4bf699d50a83043724333f7
정세영 <wjdtp93@gmail.com>
2022-06-08 14:57:21 +0900

    test: Fix test that create post

--- test/post.e2e-spec.ts
+++ test/post.e2e-spec.ts
@@ -7,6 +7,7 @@ import { join } from 'path';
 
 describe('Post Controller test', () => {
   let app: INestApplication;
+  let token = 'Bearer ';
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,10 +16,11 @@ describe('Post Controller test', () => {
 
     app = module.createNestApplication();
     await app.init();
+
+    token += process.env.TOKEN;
   });
 
   afterAll(async () => {
-    console.log(join(__dirname, '../../public'));
     rmSync(join(__dirname, '../public'), { force: true, recursive: true });
     app.close();
   });
@@ -26,6 +28,7 @@ describe('Post Controller test', () => {
   it('/posts (POST)', async () => {
     const response = await request(app.getHttpServer())
       .post('/posts')
+      .set('authorization', token)
       .field('title', 'title')
       .field('contents', 'contents')
       .field('address', 'address')
