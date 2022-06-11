d738d7907269475bdd4c495150a34e1437d842b6
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-11 10:43:34 +0900

    feat: Add validation (#37)
    * Add class-validator
    
    * Add ValidationPipe in main
    
    * Add validation in PostDto
    
    * Add class-transformer
    
    * Set validation
    
    * test: Add validation test

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class PostReq {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   contents: string;
+
+  @IsString()
+  @IsNotEmpty()
   address: string;
 }
