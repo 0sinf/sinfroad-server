fafb452a5bcf4290b3a187f759c3c86351bf76d8
정세영 <wjdtp93@gmail.com>
2022-06-14 15:33:08 +0900

    test: Add validator test in user

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -51,6 +51,17 @@ describe('User Controller test', () => {
     expect(response.body.id).toBeDefined();
   });
 
+  it('/users (POST) BadRequest', async () => {
+    const response = await request(app.getHttpServer()).post('/users').send({
+      email: '',
+      passowrd: 'password',
+      passwordConfirm: 'password',
+    });
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body.message).toBeDefined();
+  });
+
   it('/users/login (POST)', async () => {
     const response = await request(app.getHttpServer())
       .post('/users/login')
@@ -58,5 +69,17 @@ describe('User Controller test', () => {
 
     expect(response.statusCode).toEqual(201);
     expect(response.body.token).toBeDefined();
+  });
+
+  it('/users/login (POST) BadRequest', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({
+        email: '',
+        password: 'password',
+      });
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body.message).toBeDefined();
   });
 });
