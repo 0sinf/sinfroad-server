4e7249c1e2ce5d60d31f0579c7f71b2b38aeeb1b
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-14 13:32:49 +0900

    Test/after all (#38)
    * test: Use mock service in user test
    
    * test: Add mock in post test

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -2,15 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { UserService } from '../src/user/user.service';
 
 describe('User Controller test', () => {
   let app: INestApplication;
   let user: { email: string; password: string };
+  const userService = {
+    createUser: () => ({
+      id: 'sdlfkjklsdfl',
+      email: 'email@eee.com',
+      password: 'password',
+      grade: 1,
+    }),
+    loginUser: () => 'token',
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(UserService)
+      .useValue(userService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
