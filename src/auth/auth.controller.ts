b65b0bf013b52cd598c27ae756337cc985a33e4c
정세영 <wjdtp93@gmail.com>
2022-06-17 13:58:18 +0900

    feat: Add google login redirect

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  googleRedirect() {
+    // redirect google login
+  }
+}
