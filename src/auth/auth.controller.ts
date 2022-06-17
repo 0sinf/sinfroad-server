e6d841c909ac1a576ca7f75362330a5def84cee1
정세영 <wjdtp93@gmail.com>
2022-06-17 14:32:05 +0900

    feat: Add sending token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { UserService } from '../user/user.service';
 import { GoogleUser } from '../@types/user';
+import { JwtPayload } from '../@types/auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
@@ -16,11 +21,20 @@ export class AuthController {
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  async googleCallback(@Req() req: Request) {
+  async googleCallback(@Req() req: Request, @Res() res: Response) {
     const user = await this.userService.findByProviderIdOrSave(
       req.user as GoogleUser,
     );
 
     // TODO: send Access token and Refresh Token
+    const payload: JwtPayload = { sub: user.id, email: user.email };
+
+    const { accessToken, refreshToken } = this.authService.getToken(payload);
+
+    res.cookie('access-token', accessToken);
+    res.cookie('refresh-token', refreshToken);
+
+    // FIXME: Fix redirect url
+    res.redirect('/');
   }
 }
