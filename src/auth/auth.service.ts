821ce665647f6776bcefd811777562f75ff70628
정세영 <wjdtp93@gmail.com>
2022-06-17 14:16:15 +0900

    remove: Remove unnessary lib

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import * as jwt from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {
-  private key: string;
-
-  constructor() {
-    this.key = process.env.JWT_SECRET;
-  }
-
-  getToken(payload: { userId: string }): string {
-    const token = jwt.sign(payload, this.key, { expiresIn: '7d' });
-
-    return token;
-  }
-
-  verifyToken(token: string) {
-    try {
-      const payload = jwt.verify(token, this.key) as
-        | jwt.JwtPayload
-        | (string & { userId: string });
-      const { userId } = payload;
-      return userId;
-    } catch (e) {
-      throw new UnauthorizedException();
-    }
-  }
-}
+export class AuthService {}
