513fe06e8dc84308e3dc91e4c396a543ee693c59
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-18 09:56:26 +0900

    feat: Add refresh token (#48)
    * chore: Add passport-jwt lib
    
    * chore: auth types
    
    * feat: Add sending token
    
    * feat: Add google strategy in AuthModule
    
    * feat: Store hashed refresh token in user
    
    * feat: Refresh token
    
    * feat: Add refresh

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { UserService } from '../user/user.service';
 import { GoogleUser } from '../@types/user';
+import { JwtPayload } from '../@types/auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
@@ -16,11 +21,41 @@ export class AuthController {
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  async googleCallback(@Req() req: Request) {
+  async googleCallback(@Req() req: Request, @Res() res: Response) {
     const user = await this.userService.findByProviderIdOrSave(
       req.user as GoogleUser,
     );
 
-    // TODO: send Access token and Refresh Token
+    const payload: JwtPayload = { sub: user.id, email: user.email };
+
+    const { accessToken, refreshToken } = this.authService.getToken(payload);
+
+    res.cookie('access-token', accessToken);
+    res.cookie('refresh-token', refreshToken);
+
+    await this.userService.updateHashedRefreshToken(user.id, refreshToken);
+
+    // FIXME: Fix redirect url
+    res.redirect('/');
+  }
+
+  @Post('refresh')
+  @UseGuards(AuthGuard('jwt-refresh'))
+  async refreshToken(@Req() req: Request, @Res() res: Response) {
+    const { refreshToken, sub, email } = req.user as JwtPayload & {
+      refreshToken: string;
+    };
+
+    const user = await this.userService.findByIdAndCheckRT(sub, refreshToken);
+
+    const token = this.authService.getToken({ sub, email });
+
+    res.cookie('access-token', token.accessToken);
+    res.cookie('refresh-token', token.refreshToken);
+
+    await this.userService.updateHashedRefreshToken(user.id, refreshToken);
+
+    // FIXME: Fix redirect url
+    res.redirect('/');
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { GoogleStrategy } from './stratgies/google.strategy';
+import { RtStrategy } from './stratgies/refresh.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, GoogleStrategy, RtStrategy],
 })
 export class AuthModule {}
