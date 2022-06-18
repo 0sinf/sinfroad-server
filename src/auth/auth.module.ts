240cb03544be1e7ea2ebee64b0b7bba3b64cac97
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-18 11:37:18 +0900

    feat: Add jwt strategy (#49)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { GoogleStrategy } from './stratgies/google.strategy';
 import { RtStrategy } from './stratgies/refresh.strategy';
+import { AtStrategy } from './stratgies/access.strategy';
 
 @Module({
   imports: [UserModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, RtStrategy],
+  providers: [AuthService, GoogleStrategy, RtStrategy, AtStrategy],
 })
 export class AuthModule {}
---
+++ src/auth/stratgies/access.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../../@types/auth';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    // TODO: When posting, should be admin
+    // TODO: Others, can everyone
+    return payload;
+  }
+}
