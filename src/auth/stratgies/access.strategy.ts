d2c69aa688aad07a5636dc9e31cce624aa7dc7e1
정세영 <wjdtp93@gmail.com>
2022-06-20 13:53:50 +0900

    feat: Fix req.user info

--- src/auth/stratgies/access.strategy.ts
+++ src/auth/stratgies/access.strategy.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../../@types/auth';
+import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(private userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
     });
   }
 
-  validate(payload: JwtPayload) {
-    // TODO: When posting, should be admin
-    // TODO: Others, can everyone
-    return payload;
+  async validate(payload: JwtPayload) {
+    const { sub } = payload;
+
+    const user = await this.userService.findById(sub);
+
+    return user;
   }
 }
