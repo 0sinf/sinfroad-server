b6f867899b96c923ab7cb4c5a3380c59e98cf4e6
정세영 <wjdtp93@gmail.com>
2022-06-29 18:23:19 +0900

    feat: Add get user info

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,26 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { Request } from 'express';
-import { AtGuard } from 'src/common/guards';
+import { AtGuard } from '../common/guards';
+import { User } from '../@types/user';
 
 @Controller('users')
 export class UserController {
   @Get(':userId')
   @UseGuards(AtGuard)
-  async getUser(@Req() req: Request, @Param('userId') userId: string) {}
+  async getUser(@Req() req: Request, @Param('userId') userId: string) {
+    const user = req.user as User;
+
+    if (user.id !== userId) {
+      throw new UnauthorizedException();
+    }
+
+    return { user: { id: user.id, email: user.email, name: user.name } };
+  }
 }
