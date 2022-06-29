ed30c3071359ea14d04094b181d45d712930214c
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-30 08:51:48 +0900

    feat: Add getting user information (#55)
    * feat: Add User controller
    
    * feat: Add get user info
    
    * fix: Add role in response
    
    * test: Add test for getting user info

---
+++ src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { AtGuard } from '../common/guards';
+import { User } from '../@types/user';
+
+@Controller('users')
+export class UserController {
+  @Get(':userId')
+  @UseGuards(AtGuard)
+  async getUser(@Req() req: Request, @Param('userId') userId: string) {
+    const user = req.user as User;
+
+    if (user.id !== userId) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.grade,
+      },
+    };
+  }
+}
