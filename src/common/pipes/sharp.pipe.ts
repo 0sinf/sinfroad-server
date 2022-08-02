5003e98afe630ccb6057d347472a376155992fbb
정세영 <wjdtp93@gmail.com>
2022-08-02 16:15:03 +0900

    feat: Add resizing image using sharp

---
+++ src/common/pipes/sharp.pipe.ts
@@ -0,0 +1,38 @@
+import { Injectable, PipeTransform } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import * as path from 'path';
+import * as sharp from 'sharp';
+
+@Injectable()
+export class SharpPipe
+  implements
+    PipeTransform<Express.Multer.File[], Promise<Express.Multer.File[]>>
+{
+  async transform(
+    images: Express.Multer.File[],
+  ): Promise<Express.Multer.File[]> {
+    const newFilename = [];
+
+    for (let i = 0; i < images.length; i++) {
+      const filename = this.resizeFile(images[i]);
+
+      newFilename.push(filename);
+    }
+
+    return newFilename;
+  }
+
+  private async resizeFile(image: Express.Multer.File) {
+    const file = path.join(__dirname, '../../..', image.path);
+    const filename = this.generateRandomFilename(image);
+    const filePath = path.join(__dirname, '../../../static/public', filename);
+
+    await sharp(file).resize(500, 500).withMetadata().toFile(filePath);
+
+    return filename;
+  }
+
+  private generateRandomFilename(file: Express.Multer.File): string {
+    return `${uuid()}${path.extname(file.originalname)}`;
+  }
+}
