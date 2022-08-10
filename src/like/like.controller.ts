ae08eb3c18a91ee06b132c4c310eaf1b9270a438
정세영 <wjdtp93@gmail.com>
2022-08-10 23:06:14 +0900

    chore: Add to do

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -9,12 +9,26 @@ export class LikeController {
   @Post('likes')
   async addLike(@Req() req: Request, @Body() { postId }: LikeReq) {
     const user = req.user;
+
+    // TODO: Check exist like
+
+    // TODO: Check exist post
+
+    // TODO: add like entity with postId, userId
+
     throw new Error('Not implement');
   }
 
   @Delete('likes')
   async removeLike(@Req() req: Request, @Body() { postId }: LikeReq) {
     const user = req.user;
+
+    // TODO: Check exist like
+
+    // TODO: Check exist post
+
+    // TODO: remove like entity with postId, userId
+
     throw new Error('Not implement');
   }
 }
