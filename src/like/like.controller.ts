b7f9955d65c96b68923fb744cd58f2968884f0ad
정세영 <wjdtp93@gmail.com>
2022-08-10 23:04:42 +0900

    chore: Add params in controller

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Delete, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Post, UseGuards, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AtGuard } from '../common/guards/at.guard';
+import { LikeReq } from './dto/like.dto';
 
 @Controller()
 @UseGuards(AtGuard)
 export class LikeController {
   @Post('likes')
-  async addLike() {
+  async addLike(@Req() req: Request, @Body() { postId }: LikeReq) {
+    const user = req.user;
     throw new Error('Not implement');
   }
 
   @Delete('likes')
-  async removeLike() {
+  async removeLike(@Req() req: Request, @Body() { postId }: LikeReq) {
+    const user = req.user;
     throw new Error('Not implement');
   }
 }
