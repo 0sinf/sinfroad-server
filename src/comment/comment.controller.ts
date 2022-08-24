b19b5a62edfe8cd7a6a488b673b0958facd68ad6
정세영 <wjdtp93@gmail.com>
2022-08-25 08:42:41 +0900

    chore: Get userId in Request

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -2,23 +2,34 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   Param,
   Post,
   Req,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { CreateCommentReq } from './dto/create-comment.dto';
 import { CommentService } from './comment.service';
 import { AtGuard } from '../common/guards/at.guard';
 import { UserEntity } from '../user/user.entity';
+import { GetUserInterceptor } from 'src/common/interceptors/get-user.interceptor';
 
 @Controller('comments')
-@UseGuards(AtGuard)
 export class CommentController {
   constructor(private commentService: CommentService) {}
 
+  @Get()
+  @UseInterceptors(GetUserInterceptor)
+  async getComments(@Req() req: Request) {
+    // TODO: Get postId, userId, page
+    const userId = req.user && String(req.user);
+    throw new Error('Not Implement');
+  }
+
   @Post()
+  @UseGuards(AtGuard)
   async createComment(
     @Req() req: Request,
     @Body() { contents, postId }: CreateCommentReq,
@@ -35,6 +46,7 @@ export class CommentController {
   }
 
   @Delete(':id')
+  @UseGuards(AtGuard)
   async deleteComment(@Req() req: Request, @Param('id') commentId: string) {
     const user = req.user as UserEntity;
 
