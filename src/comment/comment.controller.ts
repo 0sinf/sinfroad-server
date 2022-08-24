780f35e81a6900ad504f1517cc18f59e3458ad92
정세영 <wjdtp93@gmail.com>
2022-08-24 11:50:45 +0900

    chore: Add todo

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -5,6 +5,8 @@ import { CreateCommentReq } from './dto/create-comment.dto';
 export class CommentController {
   @Post()
   async createComment(@Body() { contents, postId }: CreateCommentReq) {
+    // TODO: Get Post, User
+    // TODO: Create Comment
     throw new Error('Not Implement');
   }
 }
